18e064bc23b9a84b88a1999e0a690ebadf4d5d3c
shreyash kaushal <shreyashkaushal10@gmail.com>
2019-12-16 18:49:47 +0530

    committed on 16-12-2019

---
+++ dayofweek.js
@@ -0,0 +1,47 @@
+const readline = require('readline-sync');
+const d = readline.question('Enter the day ');
+const m = readline.question('Enter the month ');
+const y = readline.question('Enter the year ');
+
+ dayOfWeek=(m,d,y)=>{
+    let y0 =Math.round(y -(14-m)/12) 
+   // console.log(y0);
+    let x = Math.round(y0 + y0/4 -y0/100 + y0/400);
+  //  console.log(x);
+    let m0 = Math.round(m + 12*((14-m)/12) - 2);
+   // console.log(m0);
+    let d0 = Math.round((d + x + (31*m0/12))%7)
+   // console.log(d0);
+    return d0 
+}
+
+let result = dayOfWeek(m,d,y);
+//console.log(result)
+switch(result)
+{
+    case 0 :
+        day = 'Sunday';
+        break;
+    case 1 :
+        day = 'Monday';
+        break;
+    case 2 :
+        day = 'Tuesday';
+        break;
+    case 3 :
+        day = 'Wednesday';
+        break;
+    case 4 :
+        day = 'Thursday';
+        break;
+    case 5 :
+        day = 'Friday';
+        break;
+    case 6 :
+        day = 'Saturday';
+        break;
+    default: 
+        text = "No day found ";
+                                                    
+}
+console.log('Day is '+day);
--- prime.js
+++ prime.js
@@ -1,5 +1,5 @@
 
-function Prime (num) {
+Prime = (num)=> {
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
       return false
@@ -8,15 +8,41 @@ function Prime (num) {
   return true
 }
 
-function display (n) {
+ display =  (n)=> {
   const result = [2]
   for (let i = 3; i < n; i += 2) {
     if (Prime(i)) {
       result.push(i)
     }
   }
-  console.log(result) 
+  return result;
 }
-
-// display(20);
-display(1000)
+let input = display(1000)
+console.log(input)
+ Palindrome=(input)=>
+	{
+    
+		let rem, temp, final = 0;
+   	temp = input;
+		while(input>0)
+		{
+			rem = input%10;
+			input = parseInt(input/10);
+			final = final*10+rem;
+		}
+		if(final==temp)
+		{
+			return true
+		}
+  
+  }
+  let result2=[]
+  for(let i = 0;i<input.length;i++)
+  {
+    if(Palindrome(input[i]))
+    {
+      result2.push(input[i]);
+    }
+  }
+  console.log(result2)
+ 
